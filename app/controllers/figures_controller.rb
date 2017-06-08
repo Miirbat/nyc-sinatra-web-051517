1d06697ca7ceda3da08fadb8e5d2ecb3908e9b89
rkoko <rinda.ko@gmail.com>
2017-06-08 14:39:29 -0400

    routes

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,33 @@
 class FiguresController < ApplicationController
+#CRUD
+
+  get '/' do
+    erb :'/application/root'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    erb :'landmarks/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params["landmark"]["name"].empty?
+      @landmark = Landmark.create(name: params["landmark"]["name"])
+      @figure.landmarks << @landmark
+    end
+      @figure.save
+      redirect to "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params["figure"])
+    if !params["landmark"]["name"].empty?
+      @landmark = Landmark.create(name: params["landmark"]["name"])
+      @figure.landmarks << @landmark
+    end
+    erb :show
+  end
 
 end
